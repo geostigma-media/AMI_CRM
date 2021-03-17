c83f225e75c770fbfd863d0368f154cfffb02996
Mplaza005 <mplaza005@gmail.com>
2021-03-17 11:33:08 -0500

    Adicionado validacion de tamaño de imagen al formulario de contrato

--- public/js/validacion.js
+++ public/js/validacion.js
@@ -1,3 +1,25 @@
+jQuery.validator.addMethod(
+  "filesize_max",
+  function (value, element, param) {
+    var isOptional = this.optional(element),
+      file;
+
+    if (isOptional) {
+      return isOptional;
+    }
+
+    if ($(element).attr("type") === "file") {
+      if (element.files && element.files.length) {
+        file = element.files[0];
+
+        const size = file.size / 1024;
+        return size && size <= param;
+      }
+    }
+    return false;
+  },
+  "La foto no puede pesar mas de 4 MegaBytes"
+);
 if ($("#formResidente").length > 0) {
   $("#formResidente").validate({
     rules: {
@@ -128,9 +150,18 @@ if ($("#contratoForm").length > 0) {
       phone: { required: true },
       empresa_nombre: { required: true },
       empresa_direccion: { required: true },
-      front_photo_document: { required: true },
-      back_photo_document: { required: true },
-      sign_photo: { required: true },
+      front_photo_document: {
+        required: true,
+        filesize_max: 4096,
+      },
+      back_photo_document: {
+        required: true,
+        filesize_max: 4096,
+      },
+      sign_photo: {
+        required: true,
+        filesize_max: 4096,
+      },
       referencia_nombre: { required: true },
       referencia_direccion: { required: true },
       referencia_telefono: { required: true },
